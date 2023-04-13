1dd095d798c8391e2c99b6ae6df795b331c4605e
Shivanshu Mall <shivanshumall786@gmail.com>
2023-04-13 18:19:19 +0530

    Created exception class

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask
 from housing.logger import logging
+import sys
+from housing.exception import HousingException
 app=Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 def index():
-    logging.info('this is my first logging')
+    logging.info('Raising exception intentionally')
+    try:
+        a=1/0
+    
+    except Exception as e:
+        housing=HousingException(e,sys)
+        logging.info(housing.error_message)
+        
+        raise HousingException(e,sys)
+
     return 'ci cd pipeline'
 if __name__=='__main__':
     app.run(debug=True)
